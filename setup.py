fbd6c24e3d96e8f07d87ed7b6562c8e5289c713c
theVersan <158575755+daviversan@users.noreply.github.com>
2025-07-22 16:54:21 -0300

    feat: requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Info-Retrieval Project',
+    version= '0.0.0',
+    author= 'Davi Versan',
+    author_email= 'davi.versan@sou.inteli.edu.br',
+    packages= find_packages(),
+    install_requires = []
+
+)
